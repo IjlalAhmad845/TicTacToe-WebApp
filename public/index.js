c3fee8a37d1b7e6d085b9dc9d9f1b230d7f66d29
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-04-19 00:01:34 +0530

    fixed lock click events bug in p2p mode

--- public/index.js
+++ public/index.js
@@ -1,31 +1,31 @@
 import placeMarker from "./controllers/markerController.js";
 import AIMove from "./controllers/AIController.js";
 
-let isAIplayed = false;
 $(".grid-element").click(function () {
   let index = this.id.substr(this.id.length - 1) - 1;
 
-  if (!isPlaceEmpty(index)) return;
+  //check if the index is empty
+  if (!isIndexEmpty(index)) return;
+
   const whoWon = placeMarker(index);
   if (whoWon === "X" || whoWon === "O" || whoWon === "draw") {
     //game finished
     return;
   }
 
-  //fix async problem
-  //if AI is not played yet player should not be able to click on the grid
-  $("#right").css("pointer-events", "none");
-
   if ($("#mode")[0].selectedIndex === 1) {
+    //fix async problem
+    //if AI is not played yet player should not be able to click on the grid
+    $("#right").css("pointer-events", "none");
+
     setTimeout(() => {
       AIMove();
       $("#right").css("pointer-events", "auto");
-      isAIplayed = true;
     }, 500);
   }
 });
 
-const isPlaceEmpty = (index) => {
+const isIndexEmpty = (index) => {
   if (board[index] === "") return true;
   return false;
 };
