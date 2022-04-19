9df09ce4c690b276f55a3a3fad239288fe07358e
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-04-19 15:08:33 +0530

    improved game finishing logic

--- public/utils/finishHandler.js
+++ public/utils/finishHandler.js
@@ -1,52 +1,25 @@
 //telling the user who won
 const isFinished = (board) => {
   let winner = null;
-  if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
-    return board[0];
-  } else if (
-    board[3] !== "" &&
-    board[3] === board[4] &&
-    board[4] === board[5]
-  ) {
-    return board[3];
-  } else if (
-    board[6] !== "" &&
-    board[6] === board[7] &&
-    board[7] === board[8]
-  ) {
-    return board[6];
-  } else if (
-    board[0] !== "" &&
-    board[0] === board[3] &&
-    board[3] === board[6]
-  ) {
-    return board[0];
-  } else if (
-    board[1] !== "" &&
-    board[1] === board[4] &&
-    board[4] === board[7]
-  ) {
-    return board[1];
-  } else if (
-    board[2] !== "" &&
-    board[2] === board[5] &&
-    board[5] === board[8]
-  ) {
-    return board[2];
-  } else if (
-    board[0] !== "" &&
-    board[0] === board[4] &&
-    board[4] === board[8]
-  ) {
-    return board[0];
-  } else if (
-    board[2] !== "" &&
-    board[2] === board[4] &&
-    board[4] === board[6]
-  ) {
-    return board[2];
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      winner = board[a];
+    }
   }
 
+  //checking for draw
   if (winner === null && hasOpenSpots(board) === false) {
     winner = "draw";
   }
@@ -63,52 +36,24 @@ const hasOpenSpots = (board) => {
 
 //tells which position is matched
 const whichPosition = (board) => {
-  if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
-    return 1;
-  } else if (
-    board[3] !== "" &&
-    board[3] === board[4] &&
-    board[4] === board[5]
-  ) {
-    return 2;
-  } else if (
-    board[6] !== "" &&
-    board[6] === board[7] &&
-    board[7] === board[8]
-  ) {
-    return 3;
-  } else if (
-    board[0] !== "" &&
-    board[0] === board[3] &&
-    board[3] === board[6]
-  ) {
-    return 4;
-  } else if (
-    board[1] !== "" &&
-    board[1] === board[4] &&
-    board[4] === board[7]
-  ) {
-    return 5;
-  } else if (
-    board[2] !== "" &&
-    board[2] === board[5] &&
-    board[5] === board[8]
-  ) {
-    return 6;
-  } else if (
-    board[0] !== "" &&
-    board[0] === board[4] &&
-    board[4] === board[8]
-  ) {
-    return 7;
-  } else if (
-    board[2] !== "" &&
-    board[2] === board[4] &&
-    board[4] === board[6]
-  ) {
-    return 8;
-  } else {
-    return -1;
+  const lines = [
+    [0, 1, 2], //position 1
+    [3, 4, 5], //position 2
+    [6, 7, 8], //position 3
+    [0, 3, 6], //position 4
+    [1, 4, 7], //position 5
+    [2, 5, 8], //position 6
+    [0, 4, 8], //position 7
+    [2, 4, 6], //position 8
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      //returning bar position
+      return i + 1;
+    }
   }
+  return -1;
 };
 export { isFinished, whichPosition };
